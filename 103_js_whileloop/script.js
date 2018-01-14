c65964d414a80c328ffc94b46d95a1a7be82959a
Marlon McPherson <marlon.c.mcpherson@gmail.com>
2018-01-14 16:17:19 -0700

    adding new exercise

---
+++ 103_js_whileloop/script.js
@@ -0,0 +1,24 @@
+var numOne = -10;
+while (numOne<=19){
+	console.log(numOne);
+	numOne++;
+}
+var numTwo = 8;
+while (numTwo<40){
+	console.log(numTwo+2);
+	numTwo+=2;
+}
+var numThree = 300;
+while (numThree<=333){
+	if ((numThree % 2) != 0){
+		console.log(numThree);
+	}
+	numThree++;
+}
+var numFour = 5;
+while (numFour<50){
+	if (numFour % 3 == 0 && numFour % 5 == 0){
+		console.log(numFour);
+	}
+	numFour++;
+}
