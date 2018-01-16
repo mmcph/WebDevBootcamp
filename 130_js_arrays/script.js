9e051e689962e1bbaa3ec0df38b0550b3218987f
Marlon McPherson <marlon.c.mcpherson@gmail.com>
2018-01-15 18:21:28 -0700

    adding new exercise

---
+++ 130_js_arrays/script.js
@@ -0,0 +1,33 @@
+var testarr = [1,2,3,4];
+function printReverse(){}
+	for(i=testarr.length; i>=0; i--){
+		console.log(testarr[i]);
+	}
+}
+
+function isUniform(arr){
+	for(i=0; i<arr.length; i++){
+		if(arr[0] != arr[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+function sumArray(test){
+	var sum = 0;
+	for(i=0; i<test.length; i++){
+		sum = sum + test[i];
+	}
+	return sum
+}
+
+function max(range){
+	var max = range[0];
+	for(i=0; i<range.length; i++){
+		if(range[i] > max){
+			max = range[i];
+		}
+	}
+	return max;
+}
