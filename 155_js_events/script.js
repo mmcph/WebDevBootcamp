a5cd16b310c2191ee82cd5380e095d1cd3057d0f
Marlon McPherson <marlon.c.mcpherson@gmail.com>
2018-02-04 15:45:56 -0700

    adding new exercise

---
+++ 155_js_events/script.js
@@ -0,0 +1,6 @@
+var changer = document.getElementById("btn");
+
+changer.addEventListener("click", function(){
+	var changed = document.getElementById("body");
+    changed.style.background = "blue";
+});
