ea3d40f41ff3d0b4ad6887d02cd38d0138828c28
mmcph <marlon.c.mcpherson@gmail.com>
2018-06-25 21:40:12 +0000

    created version 2; added basic MongoDB interaction

---
+++ wowzone/v2/app.js
@@ -0,0 +1,70 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/wowzone");
+
+var screenshotSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Screenshot = mongoose.model("Screenshot", screenshotSchema);
+
+// Screenshot.create({
+//     name: "Westfall",
+//     image: "http://1.bp.blogspot.com/_ZnN8mGFKQxQ/TFm1xzO6BFI/AAAAAAAAAc4/VTNcnyvn7gA/s1600/intro.jpg"
+// }, function(err, screenshot){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("NEWLY CREATED SCREENSHOT: ");
+//         console.log(screenshot);
+//     }
+// });
+
+//   var screenshots = [
+//         {name: "Elwynn Forest", image: "https://i.ytimg.com/vi/uvW-QTiZLQ0/maxresdefault.jpg"},
+//         {name: "Westfall", image: "http://1.bp.blogspot.com/_ZnN8mGFKQxQ/TFm1xzO6BFI/AAAAAAAAAc4/VTNcnyvn7gA/s1600/intro.jpg"},
+//         {name: "Redridge Mountains", image: "http://3.bp.blogspot.com/_Hq85NykjWyw/TOrj4VjidKI/AAAAAAAAAu4/cIUX6I9T_Eo/s1600/redridgemountains_05.jpg"},
+//         {name: "Duskwood", image: "https://i.ytimg.com/vi/DlS2rOlNV_0/maxresdefault.jpg"}
+//       ];
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/screenshots", function(req, res){
+    Screenshot.find({}, function(err, allScreenshots){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("screenshots", {screenshots:allScreenshots});
+        }
+    });
+});
+
+app.get("/screenshots/new", function(req, res){
+   res.render("new"); 
+});
+
+app.post("/screenshots", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newScreen = {name: name, image: image};
+    Screenshot.create(newScreen, function(err, newSS){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/screenshots");
+        }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("WoWZone server started.");
+});
